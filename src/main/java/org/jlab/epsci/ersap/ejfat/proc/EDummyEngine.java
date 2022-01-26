0d5b2a70e4aac55ed080f16e134beaa5425249e8
gurjyan <gurjyan@jlab.org>
2022-01-26 10:46:45 -0500

    Dummy processing engine

---
+++ src/main/java/org/jlab/epsci/ersap/ejfat/proc/EDummyEngine.java
@@ -0,0 +1,75 @@
+package org.jlab.epsci.ersap.ejfat.proc;
+
+import org.jlab.epsci.ersap.engine.Engine;
+import org.jlab.epsci.ersap.engine.EngineData;
+import org.jlab.epsci.ersap.engine.EngineDataType;
+
+import java.util.Set;
+
+/**
+ * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
+ * See LICENSE.txt file.
+ * Thomas Jefferson National Accelerator Facility
+ * Experimental Physics Software and Computing Infrastructure Group
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ * @author gurjyan on 1/26/22
+ * @project ersap-ejfat
+ */
+public class EDummyEngine implements Engine {
+    @Override
+    public EngineData configure(EngineData input) {
+        return null;
+    }
+
+    @Override
+    public EngineData execute(EngineData input) {
+        return null;
+    }
+
+    @Override
+    public EngineData executeGroup(Set<EngineData> inputs) {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getInputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getOutputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
