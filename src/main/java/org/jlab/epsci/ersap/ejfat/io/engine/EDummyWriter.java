e82db28748453400bcc2e067d7c4cd48af1d1b9a
gurjyan <gurjyan@jlab.org>
2022-01-26 10:38:28 -0500

    DummyWriter engine

---
+++ src/main/java/org/jlab/epsci/ersap/ejfat/io/engine/EDummyWriter.java
@@ -0,0 +1,51 @@
+package org.jlab.epsci.ersap.ejfat.io.engine;
+
+import org.jlab.epsci.ersap.engine.EngineDataType;
+import org.jlab.epsci.ersap.std.services.AbstractEventWriterService;
+import org.jlab.epsci.ersap.std.services.EventWriterException;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
+ * See LICENSE.txt file.
+ * Thomas Jefferson National Accelerator Facility
+ * Experimental Physics Software and Computing Infrastructure Group
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ * @author gurjyan on 1/26/22
+ * @project ersap-ejfat
+ */
+public class EDummyWriter extends AbstractEventWriterService<FileWriter> {
+    @Override
+    protected FileWriter createWriter(Path file, JSONObject opts)
+            throws EventWriterException {
+        try {
+            return new FileWriter(file.toString());
+        } catch (IOException e) {
+            throw new EventWriterException(e);
+        }
+    }
+
+    @Override
+    protected void closeWriter() {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void writeEvent(Object event) throws EventWriterException {
+    }
+
+    @Override
+    protected EngineDataType getDataType() {
+        return EngineDataType.BYTES;
+    }
+}
