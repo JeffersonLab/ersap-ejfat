988eb4d70ead08ab5d234f4ee67174431501d408
gurjyan <gurjyan@jlab.org>
2022-01-26 12:27:01 -0500

    debugging...

--- src/main/java/org/jlab/epsci/ersap/ejfat/io/engine/EClas12HipoReader.java
+++ src/main/java/org/jlab/epsci/ersap/ejfat/io/engine/EClas12HipoReader.java
@@ -46,7 +46,6 @@ public class EClas12HipoReader extends AbstractEventReaderService<HipoReader> {
 
     @Override
     public int readEventCount() throws EventReaderException {
-        System.out.println("DDD "+ reader.getEventCount());
         return reader.getEventCount();
     }
 
