0e68dcb5cd6b6aa344ba0806fef6704321646b3a
Carl Timmer <timmer@jlab.org>
2022-01-27 17:06:38 -0500

    add EjfatPacketizeEngine

---
+++ ejfat/legacy/src/main/cpp/ejfat_packetize_engine.cpp
@@ -0,0 +1,14 @@
+#include "ejfat_packetize_engine.hpp"
+
+#include <cmath>
+#include <iostream>
+
+namespace ersap {
+namespace ejfat {
+
+    void EjfatPacketizeEngine::process()
+    {
+        std::cout << "EJFAT processing..." << std::endl;
+    }
+} // end namespace ejfat
+} // end namespace ersap
---
+++ ejfat/legacy/src/main/cpp/ejfat_packetize_engine.hpp
@@ -0,0 +1,18 @@
+#ifndef ERSAP_EJFAT_ENGINE_HPP_
+#define ERSAP_EJFAT_ENGINE_HPP_
+
+#include <string>
+
+namespace ersap {
+namespace ejfat {
+
+class EjfatPacketizeEngine
+{
+public:
+    void process();
+};
+
+} // end namespace ejfat
+} // end namespace ersap
+
+#endif
