d56e871645afa15a7ca182430761911316d4c9aa
gurjyan <gurjyan@jlab.org>
2022-02-02 09:41:24 -0500

    debugging...

--- ejfat/services/src/main/cpp/ejfat_packetize_service.cpp
+++ ejfat/services/src/main/cpp/ejfat_packetize_service.cpp
@@ -76,24 +76,24 @@ ersap::EngineData EjfatPacketizeService::configure(ersap::EngineData& input)
 ersap::EngineData EjfatPacketizeService::execute(ersap::EngineData& input)
 {
 
- auto output = ersap::EngineData{};
-    time_t end = time(nullptr);
-
-    if (end - start >= 10) {
-    std::cout << "Hey " << " " << start << " "<< end << std::endl;
-    // Pull out needed items from data
-    uint32_t *i = data_cast<uint32_t*>(input);
-    uint64_t tick = ntohl(*i);
-    uint32_t bufLen = ntohl(*(i+1));
-
-    std::cout << tick;
-
-    char *buffer = reinterpret_cast<char *>(i+2);
-
-    // This always loads the shared_pointer into a new shared_ptr
-    std::atomic_load(&engine_)->process(buffer, bufLen, host, interface, mtu, port, tick);
-    start = end;
-}
+// auto output = ersap::EngineData{};
+//    time_t end = time(nullptr);
+//
+//    if (end - start >= 10) {
+//    std::cout << "Hey " << " " << start << " "<< end << std::endl;
+//    // Pull out needed items from data
+//    uint32_t *i = data_cast<uint32_t*>(input);
+//    uint64_t tick = ntohl(*i);
+//    uint32_t bufLen = ntohl(*(i+1));
+//
+//    std::cout << tick;
+//
+//    char *buffer = reinterpret_cast<char *>(i+2);
+//
+//    // This always loads the shared_pointer into a new shared_ptr
+//    std::atomic_load(&engine_)->process(buffer, bufLen, host, interface, mtu, port, tick);
+//    start = end;
+//}
 
     return input;
 }
